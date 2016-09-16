57b5cb21d25b2f0f31749c25f83ceeeadc6a8513
이나영 <iny003@naver.com>
2016-09-17 01:08:37 +0900

    add stack

---
+++ algorithmBase/src/algorithmBase/UsingList.java
@@ -0,0 +1,73 @@
+package algorithmBase;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+//kind of list 
+
+import java.util.Iterator;
+
+public class UsingList {
+
+	/*public static void main(String[] args) {
+		List listA = new ArrayList();
+//		List listB = new LinkedList();
+//		List listC = new Vector();
+//		List listD = new Stack();
+		listA.add("andy");
+		listA.add("jully");
+		listA.add(new String("candy"));
+		 
+		listA.add(1, "apple");
+		//1에 "apple" 추가
+		
+		int index = listA.indexOf("candy");
+		//candy위치값
+        listA.add(index, "luffy");
+        //candy앞에 요소 추가
+		
+		// 인덱스를 통한 조회    //하나씩 조회할때 사용
+		String element0 = listA.get(0).toString();
+		String element1 = listA.get(1).toString();
+		String element2 = listA.get(2).toString();
+		String element3 = listA.get(3).toString();
+		System.out.println(element0+","+element1+","+element2+","+element3);
+		
+		//리스트의 길이
+		int a = listA.size();
+		System.out.println(a);
+		 
+		//Iterator 통한 전체 조회
+		Iterator iterator = listA.iterator();
+		while (iterator.hasNext()) {
+		    String element = (String) iterator.next();
+		    System.out.print(element+" ");
+		}
+		System.out.println();
+		//Iterator : boolean hasNext() : 다음 반복에서 사용될 요소가 있을떄 true반환
+		//           E next() : 다음 요소 반환
+		//           void remove() : 마지막으로 반횐된 요소 제거
+		
+		//for-loop 통한 전체 조회 
+		for(Object object : listA) {
+		    String element = (String) object;
+		    System.out.print(element+" ");
+		}
+		System.out.println();
+		
+		System.out.println(listA.contains("candy"));
+		//요소 존재 확인 Boolean값 반환
+		
+		System.out.println(listA.remove(0));
+		System.out.println(listA.remove("apple"));
+		//0번째 요소 제거 및 제거하는 값 출력
+		//"apple"요소 제거 및 Boolean 값 true반환
+		
+		listA.clear();
+		//list의 모든 요소 제거
+		
+	}*/
+
+}
---
+++ algorithmBase/src/algorithmBase/UsingStack.java
@@ -0,0 +1,39 @@
+package algorithmBase;
+
+import java.util.Iterator;
+import java.util.Stack;
+//list의 한종류로 쓰인다.
+
+public class UsingStack {
+
+	public static void main(String[] args) {
+		Stack sta = new Stack();
+		
+		sta.push(12);
+		sta.push("home");
+		sta.push(12.6);
+		//stack에 순차적으로 입력
+		
+		Iterator iterator1 = sta.iterator();
+		while (iterator1.hasNext()) {
+		    System.out.print(iterator1.next()+" ");
+		}
+		System.out.println();
+		//stack에 내용 출력
+		
+		System.out.println(sta.size());
+		// stack 사이즈
+		
+        System.out.println(sta.peek());
+        // 데이터를 빼지 않고 현재 가장 위에 위치하는 데이터 확인
+		
+		Iterator iterator2 = sta.iterator();
+		while (iterator2.hasNext()) {
+		    System.out.print(sta.pop()+" ");
+		}
+		System.out.println();
+		//stack의 내용 순차적으로 내보냄
+		
+	}
+
+}
